e241c73846af77ac9fe20201afbcd0c651278dc0
pewp <117838302+pewpewbud@users.noreply.github.com>
2023-09-26 12:27:15 -0400

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,115 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+<button class="dupe">Duplicate Card</button>
+<button class="color-change">Change Color</button>
+<button class="title-change">Change Title</button>
+<button class="delete-dupe">Delete card</button>
+
+<div class="wrapper">
+  <div class="card">
+      <img src="https://destiny.wiki.gallery/images/thumb/2/27/CrotaEndArt.jpg/1200px-CrotaEndArt.jpg" alt="Image depicting a shadow of a guardian slashing down a shadow of Crota, son of Oryx, whilst standing in Crota's throne room." style="max-width: 400px">
+      <div class="container">
+        <h2>Crotas End</h2>
+        <details>
+          <p>Crota's soul may have been banished from our world, and his brood all but demolished, but Crota himself still remains. To end this threat once and for all, a new team of six Guardians needs to enter the deepest pit of the Hellmouth and infiltrate his realm, the Oversoul Throne.</p>
+        </details>
+      </div>
+    </div>
   </div>
+</div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+</style>
+/* Style button on hover and focus */
+.dupe:hover, .dupe:focus {
+  background-color: green;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 2); /*shadows*/
+  transition: 0.3s;
+  width: 500px;
+  border: solid green;
+  display: inline-block;
+  margin: 8px; /* spacing between the cards */
+  vertical-align: top;
+  box-sizing: border-box; /* Padding and borders in the width */
+  background-color: white;
+  text-align: center;
+}
+
+/* Preventing layout issues */
+.wrapper::after {
+  display: flex;
+  clear: both;
+}
+
+/* Mouse-over shadow & color */
+.card:hover {
+  box-shadow: 0 16px 24px 0 rgba(0, 0, 0, 15);
+  background-color: #ddedd3;
+}
+
+/* Padding inside the card container */
+.container {
+  padding: 8px 16px;
+  border: 5px double lightgreen;
+  margin: 1em;
+}
+
+/* Code for button: */
+.btn-wrapper {
+  margin: 24 px auto;
+}
+.btn-wrapper button {
+  font-size: 24px;
+  color: blue;
+  background-color: white;
+}
+.btn-wrapper button:hover {
+  color: white;
+  background-color: blue;
+}
+
+/* scaling code */
+
+@media screen and (max-width: 800px) {
+  button {
+    display: none;
   }
 }
-</script>
+@media screen and (max-width: 500px) {
+  div {
+    transform: scale(0.9);
+  }
+  img {
+    width: 70%;
+  }
+}
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.colorChanged {
+  background-color: green;
 }
 </style>
+
+<script>
+// create dupe
+document.querySelector(".dupe").addEventListener("click", (e) => {
+  // Clone the first card
+  const firstCard = document.querySelector(".card");
+  const clonedCard = firstCard.cloneNode(true);
+  // Append the cloned card to the wrapper
+  document.querySelector(".wrapper").appendChild(clonedCard);
+  //delete dupe
+  document.querySelector(".delete-dupe").addEventListener("click", (e) => {
+    document.querySelector(".wrapper").removeChild(clonedCard);
+  });
+});
+//change color
+document.querySelector(".color-change").addEventListener("click", (e) => {
+  document.querySelector(".card").classList.toggle("colorChanged");
+});
+//change title
+document.querySelector(".title-change").addEventListener("click", (e) => {
+  document.querySelector(".card h2").textContent = "Something Else";
+});
+</script>
